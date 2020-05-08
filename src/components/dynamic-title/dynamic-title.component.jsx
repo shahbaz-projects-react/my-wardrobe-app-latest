3035b477144a6eabd46a790cd6064eb1717e9f9c
Shahbaz <shahbazgk13@gmail.com>
2020-05-09 00:24:40 +0530

    Misc...

--- src/components/dynamic-title/dynamic-title.component.jsx
+++ src/components/dynamic-title/dynamic-title.component.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 
 const TitleUpdater = ({ title }) => {
-  console.log(title);
   return (
     <Helmet>
       <title>{`My Wardrobe | ${title ? title : "Home"}`}</title>
