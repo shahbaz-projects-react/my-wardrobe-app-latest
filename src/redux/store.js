c2901a56944c07f8d41666b47269460a5228ff67
Shahbaz <shahbazgk13@gmail.com>
2020-05-09 01:10:22 +0530

    Misc...

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ const middlewares = [];
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
-console.log(process.env.NODE_ENV === "production", middlewares);
+console.log(process.env.NODE_ENV, middlewares);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
